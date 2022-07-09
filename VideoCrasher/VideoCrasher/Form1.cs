c94bf67d0a0725d6ce58c30990621c6c3997f8e5
VapeLiminator <69034779+VapeLiminator@users.noreply.github.com>
2022-07-09 18:50:24 +0200

    WARNING! DON'T FORGET TO OPEN WITH THE OTHER EXE!
    The other code is just for the autoupdater, tho it still needs the other source's .exe file.

---
+++ VideoCrasher/VideoCrasher/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace VideoCrasher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Hide();
+
+            WebClient webClient = new WebClient();
+
+            try
+            {
+                if (!webClient.DownloadString("https://raw.githubusercontent.com/VapeLiminator/VideoCrasher/main/version.txt").Contains("4.0.1"))
+                {
+                    if (MessageBox.Show("Looks like there is an update! Do you want to download it?", "Auto-Updater 1.0", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        using (var client = new WebClient())
+                        {
+                            System.Threading.Thread.Sleep(3000);
+                            Process.Start(@".\uwu.exe");
+                            this.Close();
+                        }
+                    else
+                    { 
+                        MessageBox.Show("You're up to date!", "Info");
+                        Process.Start(@".\VideoCrasher.bat");
+                        this.Close();
+                    }
+                }
+                else 
+                {
+                    MessageBox.Show("You're up to date!", "Info");
+                    Process.Start(@".\VideoCrasher.bat");
+                    this.Close();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+        }
+    
+
